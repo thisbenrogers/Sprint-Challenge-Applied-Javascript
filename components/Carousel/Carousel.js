350d81dfe3efd0025a5a951822799c4d53d3f21d
BenR <thisbenrogers@gmail.com>
2019-05-17 12:33:17 -0500

    class Carousel
    beginning of Carousel obj

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,3 @@
-class Carousel {
-
-}
-
-let carousel = document.querySelector();
-
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
@@ -12,3 +6,18 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+class Carousel {
+  constructor(obj) {
+    this.obj = obj.carousel;
+    this.left = document.querySelector("left-button");
+    this.right = document.querySelector("right-button");
+    this.images = document.querySelectorAll(".carImg");
+    this.index = document.querySelector(
+      `.carImg[data-index="${this.tabElement.dataset.index}"]`
+    );
+  }
+}
+
+let carousel = document.querySelector(".carousel");
+carousel = new Carousel();
